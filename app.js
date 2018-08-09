423fe2fe72ae05354f4f2c310eba970369a92b78
刘劲松 <kevin.liu@ubtrobot.com>
2018-08-09 11:24:58 +0800

    commit framework of app

--- app.js
+++ app.js
@@ -1,39 +1,137 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    var that = this;  
+    
+    //  获取商城名称
+    wx.request({
+      url: that.globalData.mainDomain+ that.globalData.subDomain +'/config/get-value',
+      data: {
+        key: 'mallName'
+      },
+      success: function(res) {
+        if (res.data.code == 0) {
+          wx.setStorageSync('mallName', res.data.data.value);
+        }
+      }
+    })
+    wx.request({
+      url: that.globalData.mainDomain + that.globalData.subDomain + '/score/send/rule',
+      data: {
+        code: 'goodReputation'
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.globalData.order_reputation_score = res.data.data[0].score;
+        }
+      }
+    })
+    wx.request({
+      url: that.globalData.mainDomain + that.globalData.subDomain + '/config/get-value',
+      data: {
+        key: 'recharge_amount_min'
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.globalData.recharge_amount_min = res.data.data.value;
+        }
+      }
+    })
+    // 获取砍价设置
+    wx.request({
+      url: that.globalData.mainDomain + that.globalData.subDomain + '/shop/goods/kanjia/list',
+      data: {},
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.globalData.kanjiaList = res.data.data.result;
+        }
+      }
+    })
+    // 判断是否登录
+    let token = wx.getStorageSync('token');
+    if (!token) {
+      that.goLoginPageTimeOut()
+      return
+    }
+    wx.request({
+      url: that.globalData.mainDomain + that.globalData.subDomain + '/user/check-token',
+      data: {
+        token: token
+      },
+      success: function (res) {
+        if (res.data.code != 0) {
+          wx.removeStorageSync('token')
+          that.goLoginPageTimeOut()
         }
       }
     })
   },
-  globalData: {
-    userInfo: null
+  sendTempleMsg: function (orderId, trigger, template_id, form_id, page, postJsonString){
+    var that = this;
+    wx.request({
+      url: that.globalData.mainDomain + that.globalData.subDomain + '/template-msg/put',
+      method:'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        token: wx.getStorageSync('token'),
+        type:0,
+        module:'order',
+        business_id: orderId,
+        trigger: trigger,
+        template_id: template_id,
+        form_id: form_id,
+        url:page,
+        postJsonString: postJsonString
+      },
+      success: (res) => {
+        //console.log('*********************');
+        //console.log(res.data);
+        //console.log('*********************');
+      }
+    })
+  },
+  sendTempleMsgImmediately: function (template_id, form_id, page, postJsonString) {
+    var that = this;
+    wx.request({
+      url: that.globalData.mainDomain + that.globalData.subDomain + '/template-msg/put',
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        token: wx.getStorageSync('token'),
+        type: 0,
+        module: 'immediately',
+        template_id: template_id,
+        form_id: form_id,
+        url: page,
+        postJsonString: postJsonString
+      },
+      success: (res) => {
+        console.log(res.data);
+      }
+    })
+  },  
+  goLoginPageTimeOut: function () {
+    setTimeout(function(){
+      wx.navigateTo({
+        url: "/pages/authorize/index"
+      })
+    }, 1000)    
+  },
+  globalData:{
+    userInfo:null,
+    mainDomain:'http://127.0.0.1/:8556',
+    subDomain: "tz", // mainDomain + subDomain
+    version: "3.0.1",
+    shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
   }
+  /*
+  根据自己需要修改下单时候的模板消息内容设置，可增加关闭订单、收货时候模板消息提醒；
+  1、/pages/to-pay-order/index.js 中已添加关闭订单、商家发货后提醒消费者；
+  2、/pages/order-details/index.js 中已添加用户确认收货后提供用户参与评价；评价后提醒消费者好评奖励积分已到账；
+  3、请自行修改上面几处的模板消息ID，参数为您自己的变量设置即可。  
+   */
 })
